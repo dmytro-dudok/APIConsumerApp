8bb36ea6f94d637e7d4af7b1ac96db266af74519
Dmytro Dudok <ddudok65@gmail.com>
2020-07-04 23:36:54 +0300

    Add ASP.NET Core MVC Project
    Adds controllers to the project and configures the DI services.

---
+++ AppDataManager/Controllers/FoxController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AppDataManager.Library.DataAccess;
+using AppDataManager.Library.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppDataManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FoxController : ControllerBase
+    {
+        private readonly ImageData _data;
+
+        public FoxController(ImageData data)
+        {
+            _data = data;
+        }
+
+        // GET: api/Fox
+        [HttpGet]
+        public IEnumerable<FoxImageModel> Get()
+        {
+            return _data.GetFoxImages();
+        }
+
+        // GET: api/Fox/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Fox
+        [HttpPost]
+        public void Post(FoxImageModel item)
+        {
+            _data.SaveFoxImageRecord(item);
+        }
+
+        // PUT: api/Fox/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ AppDataManager/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AppDataManager.Models;
+using AppDataManager.Library.DataAccess;
+
+namespace AppDataManager.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ImageData _data;
+
+        public HomeController(ILogger<HomeController> logger, ImageData data)
+        {
+            _logger = logger;
+            _data = data;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_data.GetFoxImages().ToList());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
