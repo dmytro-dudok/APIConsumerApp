952ca5be5318c2167c86f66868200e6b1db8ec08
Dmytro Dudok <ddudok65@gmail.com>
2020-07-04 20:59:27 +0300

    Add WPF Project with MVVM and Logging System
    Adds user interface project and configures it with Caliburn Micro. Also includes the Simple Container DI system.
    Adds and configures logging with Log4net.

---
+++ WpfUI/Bootstrapper.cs
@@ -0,0 +1,62 @@
+﻿using WpfUI.Library;
+using WpfUI.Library.Api;
+using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfUI.Helpers;
+using WpfUI.ViewModels;
+using App.Library.Api;
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
+namespace WpfUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        private static readonly log4net.ILog log = LogHelper.GetLogger();
+
+        private readonly SimpleContainer _container = new SimpleContainer();
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+        }
+
+        protected override void Configure()
+        {
+            _container.Instance(_container)
+                .PerRequest<IFoxImageEndpoint, FoxImageEndpoint>();
+
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IApiHelper, ApiHelper>()
+                .Singleton<IFoxProcessor, FoxProcessor>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            _container.PerRequest<ShellViewModel>();
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+    }
+}
---
+++ WpfUI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,95 @@
+﻿using WpfUI.Library;
+using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App.Library.Api;
+using App.Library.Models;
+using WpfUI.Helpers;
+
+namespace WpfUI.ViewModels
+{
+    public class ShellViewModel : Screen
+    {
+        private static readonly log4net.ILog log = LogHelper.GetLogger();
+
+        private readonly IFoxProcessor _foxProcessor;
+        private readonly IFoxImageEndpoint _foxImageEndpoint;
+        private string foxImageUrl;
+        private string title;
+
+        public ShellViewModel(IFoxProcessor foxProcessor, IFoxImageEndpoint foxImageEndpoint)
+        {
+            _foxProcessor = foxProcessor;
+            _foxImageEndpoint = foxImageEndpoint;
+        }
+
+        protected override async void OnViewLoaded(object view)
+        {
+            base.OnViewLoaded(view);
+
+                await LoadImage();
+        }
+
+        public async Task LoadImage()
+        {
+            try
+            {
+                var imageUrl = await _foxProcessor.LoadRandomFox();
+
+                FoxImageUrl = imageUrl.Image;
+
+                Title = "";
+
+                log.Info("Image loaded");
+            }
+            catch (Exception ex)
+            {
+                log.Error(ex.Message);
+            }
+
+           
+        }
+
+        public string FoxImageUrl
+        {
+            get => foxImageUrl;
+            set
+            {
+                foxImageUrl = value;
+                NotifyOfPropertyChange(() => FoxImageUrl);
+            }
+        }
+
+        public void SaveToDatabase()
+        {
+
+            try
+            {
+                var item = new FoxImageModel { ImageLink = FoxImageUrl, Title = Title };
+
+                _foxImageEndpoint.SaveFoxImage(item);
+
+                log.Info("Image saved");
+            }
+            catch (Exception ex)
+            {
+                log.Error(ex.Message);
+            }
+        }
+
+        public string Title
+        {
+            get => title; 
+            set
+            {
+                title = value;
+
+                NotifyOfPropertyChange(() => Title);
+            }
+        }
+
+    }
+}
